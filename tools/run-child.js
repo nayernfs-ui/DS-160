d80eac8a7a2e1e7624b6249c605a9b503d10651e
Nayer <nayer.nfs@gmail.com>
2026-01-08 12:38:25 +0200

    chore: remove temporary debug helpers

--- tools/run-child.js
+++
@@ -1,6 +0,0 @@
-const cp = require('child_process');
-const proc = cp.spawn(process.execPath, ['tests/unit/family.test.js'], { stdio: 'pipe' });
-proc.stdout.on('data', (d) => process.stdout.write('OUT: ' + d.toString()));
-proc.stderr.on('data', (d) => process.stderr.write('ERR: ' + d.toString()));
-proc.on('close', (code) => console.log('exit', code));
-proc.on('error', (err) => console.error('spawn error', err));
